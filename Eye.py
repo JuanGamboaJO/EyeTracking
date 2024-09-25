7bcd4075f07856a25cbfce65402011b6b615b036
Juan José Gamboa ortiz <juanjosegamboauis@gmail.com>
2024-09-24 20:05:26 -0500

    Actualización Click y espera

--- Eye.py
+++ Eye.py
@@ -9,10 +9,18 @@ import os
 import numpy as np
 import copy
 import time
+from fer import FER
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 pyautogui.FAILSAFE=False
+
+def eye_aspect_ratio(eye):
+        A = np.linalg.norm(np.array(eye[1]) - np.array(eye[5]))
+        B = np.linalg.norm(np.array(eye[2]) - np.array(eye[4]))
+        C = np.linalg.norm(np.array(eye[0]) - np.array(eye[3]))
+        ear = (A + B) / (2.0 * C)
+        return ear
 
 
 def process(im):
@@ -43,6 +51,7 @@ def dataLoad(path, want = 0):
 
 
 def eyetrack(xshift = 70, yshift=60, frameShrink = 0.15):
+    print("Funcionando")
     modelx= ConvNet().to(device)
     modelx.load_state_dict(torch.load("xModels/kernel_7_36_x.plt",map_location=device))
     modelx.eval()
@@ -57,24 +66,54 @@ def eyetrack(xshift = 70, yshift=60, frameShrink = 0.15):
     smoothed_y = None
 
     webcam = cv.VideoCapture(0)
-    
+
+    EYE_CLOSED_THRESHOLD = 0.2
+    detector = FER()
     while True:
 
         ret, frame = webcam.read()
         smallframe = cv.resize(copy.deepcopy(frame), (0, 0), fy=frameShrink, fx=frameShrink)
         smallframe = cv.cvtColor(smallframe, cv.COLOR_BGR2GRAY)
         feats = face_recognition.face_landmarks(smallframe)
-   
-        if len(feats) > 0:
+        feats_totales = face_recognition.face_landmarks(frame)
+
+        if len(feats_totales):
+            left_eyet = feats_totales[0]['left_eye']
+            right_eyet = feats_totales[0]['right_eye']
+
+            left_eye_ratio = eye_aspect_ratio(left_eyet)
+            right_eye_ratio = eye_aspect_ratio(right_eyet)
+
+
+            if left_eye_ratio < EYE_CLOSED_THRESHOLD or right_eye_ratio < EYE_CLOSED_THRESHOLD:
+                print("Se Decta Ojo cerrado, Pausando movimiento por 5 segundos.")
+                time.sleep(5)
+
+
+        emotion_results = detector.detect_emotions(frame)
+        
+        if emotion_results:
+            emotion = emotion_results[0]['emotions']
+            print(emotion['happy'])
+            if emotion['happy'] > 0.5 :  # Si la probabilidad de felicidad es mayor a 0.5
+                print('Se da click')
+                pyautogui.click()
+                continue
+
+        cv.imshow("Frame", frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        if len(feats) > 0 and len(feats_totales):
 
             leBds, leCenter = maxAndMin(feats[0]['left_eye'], mult=1/frameShrink)
             reBds, leCenter = maxAndMin(feats[0]['right_eye'], mult=1/frameShrink)
 
+
             right_eye = frame[leBds[1]:leBds[3], leBds[0]:leBds[2]]
             left_eye=frame[reBds[1]:reBds[3], reBds[0]:reBds[2]]
 
-
-
+            
             right_eye = process(right_eye)
             left_eye = process(left_eye)
 
@@ -96,13 +135,9 @@ def eyetrack(xshift = 70, yshift=60, frameShrink = 0.15):
 
             pyautogui.moveTo(smoothed_x, smoothed_y)
             
-
-
-
-            cv.imshow("Frame", frame)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
-
-
-
-eyetrack()
+            
+try:
+    eyetrack()
+except Exception as e:
+     print(f"Ocurrío un error : {e}")
+     input("Presiona Enter para salir...")
